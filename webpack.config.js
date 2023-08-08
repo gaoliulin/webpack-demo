ffb46cdea50c417c11968486e9ed12ac3ada0ea6
gaoliulin <410157812@qq.com>
2023-08-09 01:18:39 +0800

    4.步骤三 配置 webpack.config.js 文件的 devServer, 并配置快捷命令

--- webpack.config.js
+++ webpack.config.js
@@ -12,16 +12,17 @@ module.exports = {
     // 出口
     output: {
         // 输出文件的文件名
-        filename: 'main.js',
+        // filename: 'main.js',
+        filename: "bundle.[hash].js",
         // 输出文件的路径，__dirname 总是指向被执行js文件的绝对路径
         path: path.resolve(__dirname, 'dist'),
-        publicPath: 'http://localhost:8080/'
+        // publicPath: 'http://localhost:8080/'
     },
     mode:'development' , //production
     plugins:[
         new CleanWebpackPlugin(), // 先清除，在打包
         new HtmlWebpackPlugin({
-            title: '2 webpack 脚手架', //  生成html的标题
+            title: 'webpack 脚手架', //  生成html的标题
             filename:'index.html', //  生成html的文件名，默认是index.html，可以添加路径比如：src/index.html
             template:'./public/index.html', // 模版的路径
             inject:'body', 
@@ -43,12 +44,25 @@ module.exports = {
         }),
     ],
     devServer: {
-        static: {
-          directory: path.join(__dirname, 'public'),
+        static: { // 服务器加载文件路径， 5.x 版本已改为  static 配置
+          directory: path.join(__dirname, 'dist'),
         },
+        // contentBase: path.join(__dirname, "dist"), // 服务器加载文件路径 ， 4.X版本的配置项， 5.x 版本已改为  static 配置
         compress: true,
         port: 9000,
         open: true,
+        proxy: {
+            '/api': {
+                target: 'https://api.github.com', // 代理地址
+                pathRewrite: {
+                '^/api': ''
+                },
+                // 默认代理服务器，会以我们实际在浏览器请求的主机名【localhost:8080】，作为代理服务器的主机名，
+                // 然后代理服务器会带上这个主机名，去请求github，然而 github是不认识 【localhost:8080】
+                //  changeOrigin: true 就是以实际代理请求发生过程中的主机名去请求，如：代理服务器的主机名
+                changeOrigin: true
+            }
+        }
     },
 
     // devServer: {
