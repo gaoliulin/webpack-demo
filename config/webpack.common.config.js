16f6ef84c76f880fead6be2bc2d9f1ab110c364e
gaoliulin <410157812@qq.com>
2023-08-15 00:12:12 +0800

    8. 步骤四 配置 ts 及  对应loader

--- config/webpack.common.config.js
+++ config/webpack.common.config.js
@@ -46,6 +46,11 @@ module.exports = {
                     ],
                 }
             },
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                exclude:/node_modules/, //排除node_modules文件夹
+            },
 
         ],
 
