0098b689a9a97fe4749f108654778e5d16d948b9
gaoliulin <410157812@qq.com>
2023-10-30 00:13:28 +0800

    3.3 步骤五 配置 eslint 3 eslint-plugin-promise

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,8 @@ module.exports = {
   },
   "plugins": [
     "html", // eslint-plugin-html
-    "node" // eslint-plugin-node
+    "node", // eslint-plugin-node
+    "promise" // eslint-plugin-promise
   ],
   // 禁用 eslint-plugin-html  插件
   "settings": {
@@ -28,10 +29,24 @@ module.exports = {
   rules: {
     "no-var": "off",
     "no-console": "off",
-    // "import/no-unresolved": [2, { "commonjs": true, "amd": true }], // 及下面几个配置为  eslint-plugin-import 
+
+    // 及下面几个配置为  eslint-plugin-import 
+    // "import/no-unresolved": [2, { "commonjs": true, "amd": true }], 
     // "import/named": 2,
     // "import/namespace": 2,
     // "import/default": 2,
     // "import/export": 1, //  `"off"` 或 `0`  `"warn"` 或 `1`   `"error"` 或 `2`
+
+    // 及下面几个配置为  eslint-plugin-promise 
+    "promise/always-return": "error",
+    "promise/no-return-wrap": "error",
+    "promise/param-names": "error",
+    "promise/catch-or-return": "error",
+    "promise/no-native": "off",
+    "promise/no-nesting": "warn",
+    "promise/no-promise-in-callback": "warn",
+    "promise/no-callback-in-promise": "warn",
+    "promise/avoid-new": "warn",
+    "promise/no-return-in-finally": "warn"
   },
 };
